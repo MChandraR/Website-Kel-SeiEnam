17e10d2062d9cb47c7263cbc7cb5389b2048a570
MChandraR <2201020103@student.umrah.ac.id>
2024-12-12 00:20:31 +0700

    Added mongodb driver

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PelayananController;
 use App\Http\Controllers\PengajuanController;
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UsersController;
 
 Route::get('/', function () {
     return view('homeView');
@@ -20,3 +21,8 @@ Route::get('/template/ahliwaris', [TemplateController::class, 'ahliWaris']);
 Route::get('/admin', [AdminController::class, 'index']);
 Route::get('/admin/permohonan', [AdminController::class, 'permohonan']);
 Route::get('/admin/login', [AdminController::class, 'loginPage']);
+
+
+
+Route::get('/api/users', [UsersController::class, 'index']);
+
